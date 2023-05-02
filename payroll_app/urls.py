81a7fa1c9d4756b5464713213423fc086b7605df
akaNiknok <austingimperial@gmail.com>
2023-05-02 08:51:59 +0800

    Create urls and initial views

--- payroll_app/urls.py
+++ payroll_app/urls.py
@@ -17,5 +17,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello_world, name="hello_world"),
+    path('', views.employees, name="employees"),
+    path('create_employee', views.create_employee, name="create_employee"),
+    path('update_employee/<int:pk>', views.update_employee, name="update_employee"),
+    path('payslips', views.payslips, name="payslips"),
+    path('view_payslip/<int:pk>', views.view_payslip, name="view_payslip"),
 ]
--- payroll_app/views.py
+++ payroll_app/views.py
@@ -2,5 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def hello_world(request):
-    return render(request, 'payroll_app/hello_world.html')
+def employees(request):
+    return render(request, 'payroll_app/employees.html')
+
+def create_employee(request):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, 'payroll_app/create_employee.html')
+
+def update_employee(request, pk):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, 'payroll_app/update_employee.html')
+
+def payslips(request):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, 'payroll_app/payslips.html')
+
+def view_payslip(request, pk):
+    return render(request, 'payroll_app/view_payslip.html')
